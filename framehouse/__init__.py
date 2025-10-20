3fd4dcee48b1ac5d1b520e5718ac06c7cf6e3203
framehousepress <trystan.jamison@gmail.com>
2025-10-20 14:00:19 -0500

    Add framehouse module, Streamlit demo, tests, and packaging

---
+++ framehouse/__init__.py
@@ -0,0 +1,7 @@
+"""framehouse - simple image framing and grid layout utilities."""
+
+__version__ = "0.0.1"
+
+from .core import add_caption, grid_images, make_thumbnail
+
+__all__ = ["add_caption", "grid_images", "make_thumbnail", "__version__"]
